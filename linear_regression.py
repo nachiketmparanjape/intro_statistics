d60808b2a00e1f817d671a72ced7ad73502c7733
nachiketmparanjape <nachiketm.paranjape@gmail.com>
2016-05-28 13:15:27 -0400

    Cleaned two more columns in linear_regression.py making total number of cleaned columns 3

--- linear_regression.py
+++ linear_regression.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 loansData = pd.read_csv('https://github.com/Thinkful-Ed/curric-data-001-data-sets/raw/master/loans/loansData.csv')
 
 loansData['FICO.Score'] = map(lambda x: int(x[0:3]), loansData['FICO.Range'])
+loansData['Interest.Rate'] = map(lambda x: float(x[0:-1]), loansData['Interest.Rate'])
+loansData['Loan.Length'] = map(lambda x: int(x[0:-7]), loansData['Loan.Length'])
 
 plt.figure()
 p = loansData['FICO.Score'].hist()
